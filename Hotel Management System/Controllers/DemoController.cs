162fb23643a5bbb52cc02f253cd0a9e040f461cc
chyhhwen <chyhhwen@gmail.com>
2025-11-26 22:46:47 +0800

    demo

---
+++ Hotel Management System/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel_Management_System.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string showhello()
+        {
+            return "sd";
+        }
+
+        [HttpGet]
+        public string showprice(string product = "未知商品", int? price = null)
+        {
+            return $"商品是:{product}，價格是:{price}";
+        }
+
+    }
+}
